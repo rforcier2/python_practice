95187626175a27e7e2c01af865a30a5eadc0b9f3
rforcier2 <rforcier2@gmail.com>
2019-02-01 18:05:00 -0500

    Uploading content
    Uploading primary content for beginning python. Great for beginners starting python and most content taken from a Udacity course

---
+++ main.py
@@ -0,0 +1,88 @@
+# Time to start making a web crawler:
+page = '''
+<div id="top_bin">
+  <div id="top_content" class="width960">
+    <div class="udacity float-left"><a href="http://udacity.com">Udacity</a><a href="http://youtube.com">youtube</a><a href="http://codepen.io">Codepen</a><a href="http://starlimeweb.com">starlime</a>
+     '''
+#second Link
+start_link = page.find("<a href=")
+# This will find the first link on the page
+#get the first quote and second quote after html link tag
+start_quote = page.find('"', start_link)
+end_quote = page.find('"', start_quote+1)
+
+def getNextLink(pageString):
+  #second Link
+  start_link = pageString.find("<a href=")
+  # This will find the first link on the page
+  #get the first quote and second quote after html link tag
+  start_quote = pageString.find('"', start_link)
+  end_quote = pageString.find('"', start_quote+1)
+
+  #get the string inbetween those quotes, the second link on the page
+  url = pageString[start_quote+1:end_quote]
+  print (url)
+  return url, end_quote
+
+getNextLink(page)
+
+string = "Hello my name is Ronnie Hello my name is Ronnie"
+
+def find_second(string, target):
+  first_target = string.find(target)
+  return string.find(target, first_target+1)
+
+
+
+print ( find_second(string, "Hello") )
+
+def bigger(a, b):
+  return a if a > b else b
+
+print ( bigger(5,2))
+
+def is_friend(friends_name):
+  # a function that tells if a another name is a friends'
+  # All of your friend's names start with the letter D
+  if friends_name == "" or friends_name == None:
+    print("That's not a name!")
+
+  if friends_name != "":
+    return friends_name[0] == 'D' or friends_name[0] == 'd'
+
+print(is_friend("dave"))
+print(is_friend("Freddie"))
+
+#changed rule to d or n
+
+def is_friend(friends_name):
+  # a function that tells if a another name is a friends'
+  # All of your friend's names start with the letter D
+  if friends_name == "" or friends_name == None:
+    print("That's not a name!")
+
+  if friends_name != "":
+    return friends_name[0] == 'D' or friends_name[0] == 'd' or friends_name[0] == 'N' or friends_name[0] == 'n'
+print(is_friend("Nancy"))
+
+
+def biggest(a,b,c):
+  return max(a,b,c)
+
+print( biggest(5,2,3) )
+
+def print_numbers(n):
+  i = 0
+  while i < n:
+    i += 1
+    print (i)
+
+print(print_numbers(4))
+
+def factorial(n):
+  result = 1
+  while n >= 1:
+    result = result * n
+    n = n-1
+  return result
+print(factorial(5))
