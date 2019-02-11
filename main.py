dfaedeed64a1efae94aac848fe3b5a24258bbced
Ron Forcier II <rforcier2@gmail.com>
2019-02-10 19:46:21 -0500

    Update main.py
    reducing redundancy in some code

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def daysInMonth(year,month):
         if isLeapYear(year):
             return 29
         else: return 28
-    if month==1 or month==3 or month==5 or month==5 or month==7 or month==8 or month==10 or month==12:
+    if month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
         return 31
     else:
         return 30
